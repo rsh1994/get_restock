9f66e21d7472ef769cbbbab0316157d9281baebd
seonghoon <rsh1994@naver.com>
2023-05-04 00:33:39 +0900

    [ADD] 환경 세팅

---
+++ mpq.py
@@ -0,0 +1,13 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+from config import headers, url
+
+while True:
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, "html.parser")
+    buy_button = soup.select_one(".btn-b")
+
+    if buy_button and "displaynone" not in buy_button.get("class", []):
+        print("BUY NOW!")
+    time.sleep(1)
